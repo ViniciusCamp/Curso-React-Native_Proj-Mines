05f26776980ee0c221669db290c1646c4797f651
Vinícius Oliveira C de Souza <vinicius_campos@MacBook-Air-de-Vinicius.local>
2022-05-10 16:36:26 -0300

    Projeto Mines

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+import params from './params'
+import Field from './components/Field'
+
+
+export default App => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.welcome}>Iniciando o Mines!</Text>
+      <Text style={styles.instructions}>Tamanho da grade:
+        {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1}/>
+      <Field opened nearMines={2}/>
+      <Field opened nearMines={3}/>
+      <Field opened nearMines={6}/>
+      <Field mined/>
+      <Field mined opened/>
+      <Field mined opened exploded/>
+      <Field flagged/>
+      <Field flagged opened/>
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
